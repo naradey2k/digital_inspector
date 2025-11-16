9eb344ee0503a6aeb98166c4a982a361bde63521
doksterium <56970175+naradey2k@users.noreply.github.com>
2025-11-16 11:22:53 +0500

    Update qr.py

--- qr.py
+++ qr.py
@@ -4,8 +4,8 @@ import cv2, os
 from pdf2image import convert_from_path
 from qreader import QReader
 
-pdf_path = "pdfs\АПЗ-2.pdf"
-output_dir = "qr1_detected_pages"
+pdf_path = "filename.pdf"
+output_dir = "qr_detected_pages"
 os.makedirs(output_dir, exist_ok=True)
 
 qr = QReader(model_size='s', min_confidence=0.2)
@@ -13,7 +13,7 @@ qr = QReader(model_size='s', min_confidence=0.2)
 start_time = time.time()
 
 pages = convert_from_path(pdf_path, dpi=300,
-                        poppler_path=r"D:\Downloads\Release-25.11.0-0\poppler-25.11.0\Library\bin")
+                        poppler_path=r"your_path\poppler-25.11.0\Library\bin") # I needed to install poppler for pdf2image
 
 for page_num, page in enumerate(pages, start=1):
     img = np.array(page)
@@ -29,6 +29,8 @@ for page_num, page in enumerate(pages, start=1):
     output_path = os.path.join(output_dir, f"page_{page_num}.png")
     cv2.imwrite(output_path, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
     print(f"Saved: {output_path}")
+    # Creates folder with drawn bounding boxes on pages of document
 
 end_time = time.time()
+
 print(f"Total execution time: {end_time - start_time:.2f} seconds")
